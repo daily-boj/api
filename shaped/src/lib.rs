6226cab91986d9d37a66088024efec801674cbc1
RanolP <public.ranolp@gmail.com>
2020-07-04 01:07:52 +0900

    Fix ambigiouty

--- shaped/src/lib.rs
+++ shaped/src/lib.rs
@@ -1,11 +1,13 @@
+#![warn(clippy::all)]
+
 mod action;
 mod resolver;
 mod service;
 mod shaped;
 
 pub mod provider;
+pub use crate::shaped::*;
 pub use provider::Provider;
 pub use resolver::Resolver;
 pub use route_path;
 pub use service::Service;
-pub use shaped::*;
